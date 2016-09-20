750d1aeb493740fd5329acda888c748ca28839ef
Nihki Prihadi <nihkiprihadi@yahoo.com>
2016-09-21 05:38:52 +0700

    add sitemap,about,contact,privacy,terms

--- application/modules/core/controllers/IndexController.php
+++ application/modules/core/controllers/IndexController.php
@@ -1,4 +1,6 @@
 <?php
+use Pandamp\Utility\Ip,
+	Pandamp\Utility\Formatting;
 class Core_IndexController extends Zend_Controller_Action
 {
 	public function indexAction()
@@ -18,21 +20,75 @@ class Core_IndexController extends Zend_Controller_Action
 	
 	public function privacyAction()
 	{
+		$article = News_Models_Post::fetchOne(['type' => 'policy']);
+		$title = strip_tags($article->title);
 		
+		$this->view->doctype(Zend_View_Helper_Doctype::XHTML1_RDFA);
+		$this->view->headMeta()->setProperty('og:title', $title);
+		
+		$this->view->assign('article', $article);
 	}
 	
 	public function termsAction()
 	{
+		$article = News_Models_Post::fetchOne(['type' => 'terms']);
+		$title = strip_tags($article->title);
 		
+		$this->view->doctype(Zend_View_Helper_Doctype::XHTML1_RDFA);
+		$this->view->headMeta()->setProperty('og:title', $title);
+		
+		$this->view->assign('article', $article);
 	}
 	
-	public function aboutusAction()
+	public function aboutAction()
 	{
+		$article = News_Models_Post::fetchOne(['type' => 'about']);
+		$title = strip_tags($article->title);
 		
+		$this->view->doctype(Zend_View_Helper_Doctype::XHTML1_RDFA);
+		$this->view->headMeta()->setProperty('og:title', $title);
+		
+		$this->view->assign('article', $article);
 	}
 	
-	public function contactusAction()
+	public function contactAction()
 	{
+		$request = $this->getRequest();
 		
+		if ($request->isPost()) {
+			$service = $request->getPost('mycontact_services');
+			$name = $request->getPost('mycontact_name');
+			$email = $request->getPost('mycontact_email');
+			$body = $request->getPost('mycontact_body');
+			$imnotarobot = $request->getPost('imnotarobot');
+			
+			if ($imnotarobot == 'on') {
+				$now = new \MongoDate();
+				$data = new News_Models_Post([
+					'service' => $service,
+					'type' => 'contact',
+					'status' => 'publish',	
+					'content' => (new Pandamp_Utility_Posts)->sanitize_post_content($body),
+					'date' => [
+						'create' => $now,
+					],
+					'name' => $name,
+					'email' => $email,
+					'ip' => (new Ip)->getHttpRealIp(),
+				]);
+				
+				$auth = Zend_Auth::getInstance();
+				if ($auth->hasIdentity())
+				{
+					$data->author = [
+						'id' => (string) $auth['_id']->{'$id'},
+						'name' => implode(' ', [$auth->first_name, $auth->last_name]),
+						'username' => $auth->user_name
+					];
+				}
+				
+				News_Models_Post::insert($data->export());
+			}
+		}
 	}
 }
--- application/modules/news/views/helpers/Category.php
+++ application/modules/news/views/helpers/Category.php
@@ -1,27 +1,31 @@
 <?php
 class News_View_Helper_Category extends Zend_View_Helper_Abstract
 {
-	public function category($slug=null)
+	public function category($slug=null, $isCount=FALSE)
 	{
-		$category = $this->_traverseFolder('root','',0,$slug);
+		$category = $this->_traverseFolder('root','',0,$slug,$isCount);
 		
 		return $category;
 	}
 	
-	protected function _traverseFolder($folderGuid, $sGuid, $level, $slug)
+	protected function _traverseFolder($folderGuid, $sGuid, $level, $slug, $isCount=FALSE)
 	{
 		$rowSet = (new Category_Models_Category)->fetchChildren($folderGuid);
 		$sGuid = '';
 		foreach($rowSet as $row)
 		{
 			$ss = ($slug != $row['slug']) ? '' : ' class="active"';
-			if (self::getCountPost($row['_id'])) {
-				$option = '<li'.$ss.'><a href="'.$this->view->url(['slug'=>$row['slug']],'category_category_detail').'">'.$row['name'].' <span class="badge">'.self::getCountPost($row['_id']).'</span></a></li>';
-			}
-			else
-				$option = '';
+			if (!$isCount)
+				$option = '<li'.$ss.'><a href="'.$this->view->url(['slug'=>$row['slug']],'category_category_detail').'">'.str_repeat("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", $level).$row['name'].'</a></li>';
+			else 
+				if (self::getCountPost($row['_id'])) {
+					$badge = ($isCount) ? ' <span class="badge">'.self::getCountPost($row['_id']).'</span>' : '';
+					$option = '<li'.$ss.'><a href="'.$this->view->url(['slug'=>$row['slug']],'category_category_detail').'">'.$row['name'].$badge.'</a></li>';
+				}
+				else
+					$option = '';
 			
-			$sGuid .= $option . $this->_traverseFolder($row['_id'], '', $level+1, $slug);
+			$sGuid .= $option . $this->_traverseFolder($row['_id'], '', $level+1, $slug, $isCount);
 		}
 			
 		return $sGuid;
